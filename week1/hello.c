d739e16a284c23f1b795988cfbdd66f4a394ee02
Kazuki <96904689+Kazuki123-k@users.noreply.github.com>
2024-02-06 19:06:57 +0900

    week 1 class in C

--- week1/hello.c
+++ week1/hello.c
@@ -1,6 +1,14 @@
+// it's like importing a library to make it so that certian things are available.
+//printf is not available automatially. 
+//header files.
+//standard input output library!
 #include <stdio.h>
+#include "cs50.h"
+
 
 //\n is used to create a new line. (escape sequence.)
 int main(void) {
-    printf("hello, world\n");
+    //%s is a placeholder for a variable
+    string answer = get_string("What's your name? ");
+    printf("Hello, %s\n" , answer);
 }
